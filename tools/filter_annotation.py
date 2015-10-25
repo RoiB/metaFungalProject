4472211f1ab453e0b0bb2fc7b905282dae15fb73
Richard Ding <roibcell@gmail.com>
2015-10-24 23:42:36 -0400

    perform test on filterd set

--- tools/filter_annotation.py
+++ tools/filter_annotation.py
@@ -10,4 +10,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 4:
         print "please provide both input and output file"
     else:
+        print 'input:', sys.argv[1]
+        print 'output:', sys.argv[2]
+        print 'keyword', sys.argv[3]
         filter_annotation(sys.argv[1], sys.argv[2],sys.argv[3])
